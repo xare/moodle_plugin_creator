277920bc006183c80190b029d473c1c487a2af89
xare <xaresd@gmail.com>
2023-08-18 16:05:09 +0200

    Add languages as command option

--- cli/create.php
+++ cli/create.php
@@ -6,8 +6,8 @@ require(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
 require_once($CFG->libdir.'/clilib.php');      // cli only functions
 
 list($options, $unrecognized) = cli_get_params(
-    array('help' => false, 'name' => ''),
-    array('h' => 'help', 'n' => 'name')
+    array('help' => false, 'name' => '','languages'=>''),
+    array('h' => 'help', 'n' => 'name', 'l'=>'languages')
 );
 
 if ($options['help'] || !$options['name']) {
@@ -15,10 +15,11 @@ if ($options['help'] || !$options['name']) {
 
 Options:
 -n, --name         Name of the new local plugin.
+-l, --languages    Comma separated list of language ISO Codes to create lang files for.
 -h, --help         Print out this help.
 
 Example:
-\$sudo -u www-data /usr/bin/php local/plugincreator/cli/create.php --name=newpluginname
+\$sudo -u www-data /usr/bin/php local/plugincreator/cli/create.php --name=newpluginname --languages=en,es,fr
 ";
 
     echo $help;
@@ -35,9 +36,7 @@ if (file_exists($plugindir)) {
 
 // Create the basic plugin structure
 mkdir($plugindir, 0777, true);
-mkdir($plugindir . '/db');
-mkdir($plugindir . '/classes');
-mkdir($plugindir . '/lang');
+
 file_put_contents($plugindir . '/version.php', "<?php
 
 defined('MOODLE_INTERNAL') || die();
@@ -48,5 +47,30 @@ defined('MOODLE_INTERNAL') || die();
 \$plugin->maturity = MATURITY_STABLE;
 \$plugin->release = 'v1.0.0';
 ");
+
+file_put_contents($plugindir. '/README.md'," 
+#PLUGIN ".$name."#");
+mkdir($plugindir . '/lang');
+if ($options['languages']) {
+    $langs = explode(',', $options['languages']);
+    foreach ($langs as $lang) {
+        $lang = trim($lang);
+        $langdir = $plugindir . "/lang/$lang";
+        mkdir($langdir, 0777, true);
+        
+        // Sample language string file content.
+        $langcontent = "<?php
+\$string['pluginname'] = 'My $name plugin';
+// Add more language strings as needed.
+";
+        file_put_contents($langdir . "/local_$name.php", $langcontent);
+        echo "The file for the ".$lang." language was created";
+    }
+}
+
+
+mkdir($plugindir . '/db');
+mkdir($plugindir . '/classes');
+
 
 echo "Plugin '$name' created successfully!\n";
